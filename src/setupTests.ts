27a398d7fee4e6db4db4f4248c604725cc0ca47e
gauchoscript <elio.g.gutierrez@gmail.com>
2026-03-02 20:18:41 -0300

    refactor: improve reset function naming

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,11 +1,11 @@
 import '@testing-library/jest-dom';
 import { afterAll, afterEach, beforeAll, beforeEach } from 'vitest';
-import { resetTasks } from './mocks/handlers';
+import { resetMocks } from './mocks/handlers';
 import { server } from './mocks/server';
 
 beforeAll(() => server.listen());
 beforeEach(() => {
-  resetTasks(); // Reset in-memory state
+  resetMocks(); // Reset in-memory state
 });
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
